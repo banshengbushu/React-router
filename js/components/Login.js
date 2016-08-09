1fc11b6db8ba7023f64f38a4642140bd7a7be6f0
banshengbushu <wangdanna1995@outlook.com>
2016-08-10 00:39:17 +0800

    react-router

---
+++ js/components/Login.js
@@ -0,0 +1,14 @@
+/**
+ * Created by wangdanna on 16-8-4.
+ */
+
+import React from 'react';
+
+let Login = React.createClass({
+
+    render() {
+        return(<div>Welcome to login</div>);
+    }
+});
+
+export default Login;
